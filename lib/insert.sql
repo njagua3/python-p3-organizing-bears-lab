6cba26513c8da7f3374f59942b4a306a2f1555c6
njagua <njagua.gma@gmail.com>
2024-09-16 08:09:05 +0300

    add

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+-- Insert data for Mr. Chocolate
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 10, 'F', 'White', 'choleric', true);
+
+-- Insert data for Rowdy
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 2, 'M', 'Black', 'melancholic', true);
+
+-- Insert data for Tabitha
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 4, 'M', 'White', 'choleric', true);
+
+-- Insert data for Sergeant Brown
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 9, 'F', 'White', 'choleric', true);
+
+-- Insert data for Melissa
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 1, 'F', 'White', 'choleric', true);
+
+-- Insert data for Grinch (fixed missing comma)
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 16, 'F', 'White', 'choleric', true);
+
+-- Insert data for Wendy
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 5, 'M', 'White', 'sanguine', true);
+
+-- Insert data for unnamed bear (NULL for name and age)
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, NULL, 'M', 'White', 'phlegmatic', true);
